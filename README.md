6d10225abb90eb773f31005384bfcecaa786af8c
Steve Yai <23424164+stephenyai@users.noreply.github.com>
2019-07-16 18:39:59 -0700

    final draft

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # unit-4-game
+
+##Crystal Collector 
+
+__Using **HTML**, **CSS**, and **JQuery** to create number generated game__
+
+Instructions below: 
+
+1) You will be shown a random number at the start of the game. 
+2) There are 4 crystals below. By clicking on a crystal you will add a specific amount of points to your total score. 
+3) You win the game by matching your total score to the random number. You lose if your total score goes above the random number. 			
+4) The value of each crystal is hidden until clicked. 
+5) Each time the game starts, the game will change the values of each crystal.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,39 +4,47 @@ var randomNum;
 var crystalNum;
 var userNum;
 var totalNum = 0;
-var startGame = false;
-// line 14 - Math.floor(Math.random() * (max-min)) + min;
-// line 18 - crystalNum = Math.floor(Math.random() * 12) + 1;
+var wins = 0;
+var losses = 0;
 
 function defaultPage() {
+
+	totalNum = 0;
+
 	min = Math.ceil(19);
 	max = Math.floor(121);
-	randomNum = 20
+	randomNum = Math.floor(Math.random() * (max-min)) + min;
 	$('#random').text(randomNum);
+	$('#guess').text(totalNum);
 
 	$('button').each(function() {
-		crystalNum = Math.floor(Math.random() * 3) + 1;
+		crystalNum = Math.floor(Math.random() * 12) + 1;
 		// console.log('crystl', crystalNum)
 		$(this).attr('value', crystalNum);
 	})	
 }
 
-
-
 $('button').on('click', function(event) {
 	userNum = $(this).attr('value');
 	userNum = parseInt(userNum);
-	console.log('b4', userNum)
+	totalNum = userNum + totalNum;
 
-	if (totalNum === randomNum) {
-		alert('you win');
-	} else {
-		totalNum = userNum + totalNum;
-		console.log('totalNum', totalNum)
-		console.log('userNum', userNum)
+	$('#guess').text(totalNum);
+
+	if (totalNum > randomNum) {
+		losses++;
+		$('#losses').text(losses);
+		$('#alert').text("You lose!")
+		defaultPage()
+
+	} else if (totalNum === randomNum) {
+		wins++;
+		$('#wins').text(wins);
+		$('#guess').text(totalNum);
+		$('#alert').text("You win!")
+
+		defaultPage()
 	}
 })
-
-
 
 defaultPage();
